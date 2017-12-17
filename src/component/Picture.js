005070b47d105b73cddda5936535dc8ed98da402
skyview <skyview.tian@gmail.com>
2017-12-17 16:24:13 -0500

    added polyfill for fetch. added prevent scrolling when carousel is shown. added readme

--- src/component/Picture.js
+++ src/component/Picture.js
@@ -3,33 +3,72 @@ import '../css/Picture.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import { Thumbnail, Col } from 'react-bootstrap';
 
-
 const thumbnailUrl = (farm, server, photoId, secret) =>
-`https://farm${farm}.staticflickr.com/${server}/${photoId}_${secret}.jpg`;
+  `https://farm${farm}.staticflickr.com/${server}/${photoId}_${secret}.jpg`;
 
 class Picture extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isHovering: false
+    };
+    this.openCarousel = this.openCarousel.bind(this);
+    this.handleMouseHover = this.handleMouseHover.bind(this);
+  }
 
-	constructor(props) {
-		super(props);
-		this.openCarousel = this.openCarousel.bind(this);
-	}
+  openCarousel() {
+    this.props.onPictureClick(this.props.index);
+  }
 
-	openCarousel(){
-		this.props.onPictureClick(this.props.index);
-	}
+  handleMouseHover() {
+    this.setState(this.toggleHoverState);
+  }
 
-	render() {
+  toggleHoverState(state) {
+    return {
+      isHovering: !state.isHovering
+    };
+  }
 
-		return(
-			<Col onClick={() => this.openCarousel()} xs={12} sm={6} md={4} lg={3}>
-				<span className="index-counter">{this.props.index + 1}</span>
-				<Thumbnail src={thumbnailUrl(this.props.farm, this.props.server, this.props.photoId, this.props.secret)} alt={this.props.title}>
-					<p className="image-title">{this.props.title} {this.props.info ? <span className="view-count"><i className="fa fa-eye"></i>{this.props.info.views} views</span> : null} </p>
-				</Thumbnail>
-			</Col>
-		);
-	}
-
+  render() {
+    return (
+      <Col
+        onMouseEnter={this.handleMouseHover}
+        onMouseLeave={this.handleMouseHover}
+        onClick={() => this.openCarousel()}
+        xs={12}
+        sm={6}
+        md={4}
+        lg={3}
+      >
+        <span className="index-counter">{this.props.index + 1}</span>
+        <Thumbnail
+          src={thumbnailUrl(
+            this.props.farm,
+            this.props.server,
+            this.props.photoId,
+            this.props.secret
+          )}
+          alt={this.props.title}
+        >
+          <p className="image-title">
+            {this.props.title}{' '}
+            {this.props.info ? (
+              <span className="view-count">
+                <i className="fa fa-eye" />
+                {this.props.info.views} views
+              </span>
+            ) : null}{' '}
+          </p>
+        </Thumbnail>
+        {this.state.isHovering ? (
+          <div className="on-hover">
+            <span>Click to see more details</span>
+          </div>
+        ) : null}
+      </Col>
+    );
+  }
 }
 
 export default Picture;
--- src/component/PicturesContainer.js
+++ src/component/PicturesContainer.js
@@ -6,296 +6,383 @@ import Waypoint from 'react-waypoint';
 import '../css/PicturesContainer.css';
 import { Alert, Button } from 'react-bootstrap';
 import _ from 'lodash';
-
+import 'whatwg-fetch';
 const getPicturesUrl = (apiKey, userId, pageId) =>
-`https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=${apiKey}&user_id=${userId}&page=${pageId}&format=json&nojsoncallback=1&per_page=50`;
+  `https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=${apiKey}&user_id=${userId}&page=${pageId}&format=json&nojsoncallback=1&per_page=50`;
 const getSizesUrl = (apiKey, photoId) =>
-`https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=${apiKey}&photo_id=${photoId}&format=json&nojsoncallback=1`;
+  `https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=${apiKey}&photo_id=${photoId}&format=json&nojsoncallback=1`;
 const getInfoUrl = (apiKey, photoId, secret) =>
-`https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=${apiKey}&photo_id=${photoId}&secret=${secret}&format=json&nojsoncallback=1`;
-
+  `https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=${apiKey}&photo_id=${photoId}&secret=${secret}&format=json&nojsoncallback=1`;
 
 class PicturesContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: 1,
+      error: false,
+      isloading: true
+    };
+    this.fetchMorePhotos = this.fetchMorePhotos.bind(this);
+    this.disPatchAllPromises = this.disPatchAllPromises.bind(this);
+    this.refreshPage = this.refreshPage.bind(this);
+    this.dismissError = this.dismissError.bind(this);
+    this.handleOpenCarousel = this.handleOpenCarousel.bind(this);
+    this.handleCloseCarousel = this.handleCloseCarousel.bind(this);
+    this.syncPictureListState = this.syncPictureListState.bind(this);
+    this.handleMasonryScroll = this.handleMasonryScroll.bind(this);
+  }
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			page: 1,
-			error: false
-		};
-		this.fetchMorePhotos = this.fetchMorePhotos.bind(this);
-		this.disPatchAllPromises = this.disPatchAllPromises.bind(this);
-		this.refreshPage = this.refreshPage.bind(this);
-		this.dismissError = this.dismissError.bind(this);
-		this.handleOpenCarousel = this.handleOpenCarousel.bind(this);
-		this.handleCloseCarousel = this.handleCloseCarousel.bind(this);
-		this.syncPictureListState = this.syncPictureListState.bind(this);
-	}
+  componentDidMount() {
+    window.addEventListener('wheel', this.handleMasonryScroll);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('wheel', this.handleMasonryScroll);
+  }
 
-	fetchSizes(apiKey, farm, secret, id){
-		return fetch(getSizesUrl(apiKey, id))
-			.then(response => {
-				if (!response.ok) {
-					this.setState({
-						error: true,
-						errorMessage: response.statusText
-					});
-				}
-				return response;
-			})
-			.then(d => d.json())
-			.then(d => {
-				const sizes = _.filter(d.sizes.size, (photo) =>{ 
-					return photo.label === 'Large' || photo.label === 'Original'; 
-				});
-				return {key: farm+'-'+secret+'-'+id,
-						sizes: sizes};
-			},
-			 () => {
-			this.setState({
-				error: true
-			});
-		});
-	}
+  handleMasonryScroll(event) {
+    if (this.props.isCarouselShown) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+  }
 
+  fetchSizes(apiKey, farm, secret, server, id) {
+    return fetch(getSizesUrl(apiKey, id))
+      .then(response => {
+        if (!response.ok) {
+          this.setState({
+            error: true,
+            errorMessage: response.statusText,
+            isLoading: true
+          });
+        }
+        return response;
+      })
+      .then(d => d.json())
+      .then(
+        d => {
+          const sizes = _.filter(d.sizes.size, photo => {
+            return photo.label === 'Large' || photo.label === 'Original';
+          });
+          return {
+            key: farm + '-' + secret + '-' + server + '-' + id,
+            sizes: sizes
+          };
+        },
+        () => {
+          this.setState({
+            error: true
+          });
+        }
+      );
+  }
 
-	fetchInfo(apiKey, farm, secret, id){
-		return fetch(getInfoUrl(apiKey, id, secret))
-			.then(response => {
-				if (!response.ok) {
-					this.setState({
-						error: true,
-						errorMessage: response.statusText
-					});
-				}
-				return response;
-			})
-			.then(d => d.json())
-			.then(d => {
+  fetchInfo(apiKey, farm, secret, server, id) {
+    return fetch(getInfoUrl(apiKey, id, secret))
+      .then(response => {
+        if (!response.ok) {
+          this.setState({
+            error: true,
+            errorMessage: response.statusText
+          });
+        }
+        return response;
+      })
+      .then(d => d.json())
+      .then(
+        d => {
+          let dateTakenObj = new Date(d.photo.dates.taken);
 
-				let dateTakenObj = new Date(d.photo.dates.taken);
+          return {
+            key: farm + '-' + secret + '-' + server + '-' + id,
+            info: {
+              lastupdate: {
+                numeric: _.toNumber(d.photo.dates.lastupdate),
+                display: new Date(
+                  _.toNumber(d.photo.dates.lastupdate) * 1000
+                ).toDateString()
+              },
+              posted: {
+                numeric: _.toNumber(d.photo.dates.posted),
+                display: new Date(
+                  _.toNumber(d.photo.dates.posted) * 1000
+                ).toDateString()
+              },
+              taken: {
+                numeric: (dateTakenObj.getTime() / 1000) | 0,
+                display: dateTakenObj.toDateString()
+              },
+              dateuploaded: {
+                numeric: _.toNumber(d.photo.dateuploaded),
+                display: new Date(
+                  _.toNumber(d.photo.dateuploaded) * 1000
+                ).toDateString()
+              },
+              description: d.photo.description._content,
+              owner: {
+                location: d.photo.owner.location,
+                realname: d.photo.owner.realname
+              },
+              views: _.toNumber(d.photo.views)
+            }
+          };
+        },
+        () => {
+          this.setState({
+            error: true
+          });
+        }
+      );
+  }
 
-				return {key: farm+'-'+secret+'-'+id,
-					info: {
-						lastupdate: {
-							numeric: _.toNumber(d.photo.dates.lastupdate),
-							display: (new Date(_.toNumber(d.photo.dates.lastupdate) * 1000).toDateString())
-						},
-						posted: {
-							numeric: _.toNumber(d.photo.dates.posted),
-							display: (new Date(_.toNumber(d.photo.dates.posted) * 1000).toDateString())
-						},
-						taken: {
-							numeric: dateTakenObj.getTime()/1000|0,
-							display: dateTakenObj.toDateString()
-						},
-						dateuploaded: {
-							numeric: _.toNumber(d.photo.dateuploaded),
-							display: (new Date(_.toNumber(d.photo.dateuploaded) * 1000).toDateString())
-						},
-						description: d.photo.description._content,
-						owner: {
-							location: d.photo.owner.location,
-							realname: d.photo.owner.realname
-						},
-						views: _.toNumber(d.photo.views)
-					}};
-				
-			}, () => {
-			this.setState({
-				error: true
-			});
-		});
+  disPatchAllPromises(callsArray) {
+    let stateObj = this.state.photolist;
 
-	}
+    let firstPages = stateObj.length === 50 ? [] : _.dropRight(stateObj, 50);
+    let lastPage = stateObj.length === 50 ? stateObj : stateObj.slice(-50);
+    return Promise.all(callsArray).then(response => {
+      // array order is maintained, can just do
 
+      const merged = _.merge(lastPage, response);
 
-	disPatchAllPromises(callsArray){
-		let stateObj = this.state.photolist;
+      this.setState({
+        photolist: firstPages.concat(merged)
+      });
+    });
+  }
 
-		let firstPages = stateObj.length === 50 ? [] : _.dropRight(stateObj, 50);
-		let lastPage = stateObj.length === 50 ? stateObj : stateObj.slice(-50);
-		return Promise.all(callsArray).then(response =>{
+  fetchPictures(pageId) {
+    var that = this;
+    fetch(getPicturesUrl(this.props.apiKey, this.props.userId, pageId))
+      .then(response => {
+        if (!response.ok) {
+          this.setState({
+            error: true,
+            errorMessage: response.statusText
+          });
+        }
+        return response;
+      })
+      .then(d => d.json())
+      .then(
+        data => {
+          let getSizeCalls = [];
+          let getInfoCalls = [];
+          let previousPhotos = this.state.photolist || [];
+          let photos = data.photos.photo.map(photo => {
+            getSizeCalls.push(
+              that.fetchSizes(
+                that.props.apiKey,
+                photo.farm,
+                photo.secret,
+                photo.server,
+                photo.id
+              )
+            );
+            getInfoCalls.push(
+              that.fetchInfo(
+                that.props.apiKey,
+                photo.farm,
+                photo.secret,
+                photo.server,
+                photo.id
+              )
+            );
+            return {
+              id: photo.id,
+              key:
+                photo.farm +
+                '-' +
+                photo.secret +
+                '-' +
+                photo.server +
+                '-' +
+                photo.id,
+              farm: photo.farm,
+              owner: photo.owner,
+              secret: photo.secret,
+              server: photo.server,
+              title: photo.title
+            };
+          });
 
-			// array order is maintained, can just do
+          that.setState({
+            photolist: previousPhotos.concat(photos),
+            page: data.photos.page,
+            pages: data.photos.pages,
+            isLoading: false
+          });
 
-			const merged = (_.merge(lastPage, response));
+          that.disPatchAllPromises(getSizeCalls);
+          that.disPatchAllPromises(getInfoCalls);
+        },
+        () => {
+          this.setState({
+            error: true
+          });
+        }
+      );
+  }
 
-			this.setState({
-				photolist: firstPages.concat(merged)
-			});
-		})
-	}
+  handleOpenCarousel(index) {
+    this.props.onUpdate({
+      carouselIndex: index,
+      isCarouselShown: true
+    });
+  }
 
+  handleCloseCarousel() {
+    this.props.onUpdate({ isCarouselShown: false });
+  }
 
-	fetchPictures(pageId){
-		var that = this;
-		fetch(getPicturesUrl(this.props.apiKey, this.props.userId, pageId))
-			.then(response => {
-				if (!response.ok) {
-					this.setState({
-						error: true,
-						errorMessage: response.statusText
-					});
-				}
-				return response;
-			})
-			.then(d => d.json())
-			.then(data => {
+  fetchMorePhotos() {
+    if (
+      this.state.pages === undefined ||
+      (this.state.page < this.state.pages && !this.state.isLoading)
+    ) {
+      this.fetchPictures(this.state.page + 1);
+      this.dismissError();
+    }
+  }
 
-				let getSizeCalls = [];
-				let getInfoCalls = [];
-				let previousPhotos = this.state.photolist || [];
-				let photos = data.photos.photo.map(photo =>{
-					getSizeCalls.push( that.fetchSizes(that.props.apiKey, photo.farm, photo.secret, photo.id) );
-					getInfoCalls.push( that.fetchInfo(that.props.apiKey, photo.farm, photo.secret, photo.id));
-					return {
-					   'id': photo.id,
-					   'key': photo.farm+'-'+photo.secret+'-'+photo.id,
-					   'farm': photo.farm,
-					   'owner': photo.owner,
-					   'secret': photo.secret,
-					   'server': photo.server,
-					   'title': photo.title 
-					};
-				});
+  dismissError() {
+    this.setState({
+      error: false
+    });
+  }
 
-				that.setState({
-					photolist: previousPhotos.concat(photos),
-					page: data.photos.page,
-					pages: data.photos.pages
-				});
+  refreshPage() {
+    this.setState({
+      page: 1,
+      error: false,
+      photolist: []
+    });
+    this.fetchMorePhotos();
+    this.dismissError();
+  }
 
-				that.disPatchAllPromises(getSizeCalls);
-				that.disPatchAllPromises(getInfoCalls);
-				
-			}, () => {
-			this.setState({
-				error: true
-			});
-		});
-	}
+  syncPictureListState() {
+    let that = this;
+    let filteredArray = this.state.photolist;
 
-	handleOpenCarousel(index){
-		this.props.onUpdate({
-			carouselIndex: index,
-			isCarouselShown: true
-		});
-	}
+    if (this.props.filterText && this.props.filterText !== '') {
+      filteredArray = _.filter(that.state.photolist, item => {
+        if (isNaN(that.props.filterText)) {
+          // search for title
+          return (
+            item.title
+              .toLowerCase()
+              .indexOf(that.props.filterText.toLowerCase()) !== -1
+          );
+        } else {
+          return (
+            item.id
+              .toLowerCase()
+              .indexOf(that.props.filterText.toLowerCase()) !== -1
+          );
+        }
+      });
+    }
 
-	handleCloseCarousel(){
-		this.props.onUpdate({isCarouselShown: false});
-	}
+    // don't sort when all the info hasnt arrived yet; sort will trigger when they do
+    if (
+      this.props.sortField &&
+      this.props.sortField !== '' &&
+      filteredArray[filteredArray.length - 1].info
+    ) {
+      filteredArray = _.orderBy(
+        filteredArray,
+        item => {
+          if (this.props.sortField === 'views') {
+            return item.info[that.props.sortField];
+          }
+          return item.info[that.props.sortField].numeric;
+        },
+        [that.props.isAscending ? 'asc' : 'desc']
+      );
+    }
+    return filteredArray;
+  }
 
-	componentDidMount() {
-		this.fetchPictures(this.state.page);
-	}
+  getPictureList() {
+    let that = this;
+    const filteredArray = this.syncPictureListState();
+    // console.log(filteredArray);
+    return _.map(filteredArray, (item, index) => {
+      return (
+        <Picture
+          onPictureClick={index => this.handleOpenCarousel(index)}
+          index={index}
+          key={item.farm + '-' + item.secret + '-' + item.id}
+          photoId={item.id}
+          secret={item.secret}
+          server={item.server}
+          farm={item.farm}
+          title={item.title}
+          info={item.info}
+          sizes={item.sizes}
+          apiKey={that.props.apiKey}
+        />
+      );
+    });
+  }
 
-	fetchMorePhotos(){
-		if(this.state.page < this.state.pages) {
-			this.fetchPictures(this.state.page + 1);
-			this.dismissError();
-		}
-	}
-
-	dismissError() {
-		this.setState({
-			error: false
-		})
-	}
-
-	refreshPage() {
-		this.setState({
-			page: 1,
-			error: false,
-			photolist: []
-		});
-		this.fetchMorePhotos();
-		this.dismissError();
-	}
-
-	syncPictureListState(){
-		let that = this;
-		let filteredArray = this.state.photolist;
-
-		if(this.props.filterText && this.props.filterText !== ''){
-			filteredArray = _.filter(that.state.photolist, (item) => {
-				if(isNaN(that.props.filterText)) {
-					// search for title
-					return item.title.toLowerCase().indexOf(that.props.filterText.toLowerCase()) !== -1;
-				}else{
-					return item.id.toLowerCase().indexOf(that.props.filterText.toLowerCase()) !== -1;
-				}
-			});
-		}
-
-		// don't sort when all the info hasnt arrived yet; sort will trigger when they do
-		if(this.props.sortField && this.props.sortField !== '' && filteredArray[filteredArray.length-1].info){
-
-			filteredArray = _.orderBy(filteredArray, item =>{
-				if(this.props.sortField === 'views'){
-					return item.info[that.props.sortField];
-				}
-				return item.info[that.props.sortField].numeric;
-			}, [that.props.isAscending ? 'asc' : 'desc']);
-
-		}
-		return filteredArray;
-	}
-
-	getPictureList(){
-		let that = this;
-		const filteredArray = this.syncPictureListState();
-		// console.log(filteredArray);
-		return _.map(filteredArray, (item, index) => {
-			return (
-				<Picture onPictureClick={ (index) => this.handleOpenCarousel(index)} index={index} key={item.farm+'-'+item.secret+'-'+item.id} photoId={item.id} secret={item.secret} server={item.server} farm={item.farm} title={item.title} info={item.info} sizes={item.sizes} apiKey={that.props.apiKey}></Picture>
-			);
-		});
-	}
-
-
-	render() {
-		return (
-			<div>
-				<div className="main-container">
-					{ this.state.error ?
-					<Alert bsStyle="danger" className="error-alert" onDismiss={this.dismissError}>
-					<h4>Oh snap! You got an error!</h4>
-						<p>{this.state.errorMessage ? this.state.errorMessage : 'please try again.'}</p>
-						<p className="buttons">
-							<Button bsStyle="danger" onClick={this.refreshPage}>Refresh Page</Button>
-	            			<Button onClick={this.dismissError}>Close</Button>
-	            		</p>
-	        		</Alert> : null }
-					<div>
-						<Masonry
-							className={"my-gallery-class"} 
-							disableImagesLoaded={false}
-							updateOnEachImageLoad={true}
-						>
-
-							{this.getPictureList()}
-
-						</Masonry>
-					</div>
-					<Waypoint
-						buttomOffset={'-200px'}
-						onEnter={this.fetchMorePhotos}
-					>
-						 <div className="loader"><i className="fa fa-spinner fa-spin"></i></div>
-					</Waypoint>
-				</div>
-				{ this.props.isCarouselShown ? 
-					<div className="overlay">
-						<PictureCarousel onClose={ () => this.handleCloseCarousel()} index={this.props.carouselIndex} photolist={this.syncPictureListState()}></PictureCarousel> 
-					</div>
-					: null
-				}
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <div className="main-container">
+          {this.state.error ? (
+            <Alert
+              bsStyle="danger"
+              className="error-alert"
+              onDismiss={this.dismissError}
+            >
+              <h4>Oh snap! You got an error!</h4>
+              <p>
+                {this.state.errorMessage
+                  ? this.state.errorMessage
+                  : 'please try again.'}
+              </p>
+              <p className="buttons">
+                <Button bsStyle="danger" onClick={this.refreshPage}>
+                  Refresh Page
+                </Button>
+                <Button onClick={this.dismissError}>Close</Button>
+              </p>
+            </Alert>
+          ) : null}
+          <div>
+            <Masonry
+              ref="masonry"
+              className={'my-gallery-class'}
+              disableImagesLoaded={false}
+              updateOnEachImageLoad={true}
+            >
+              {this.getPictureList()}
+            </Masonry>
+          </div>
+          {(this.props.filterText === undefined ||
+            this.props.filterText === '') &&
+          !this.props.isCarouselShown ? (
+            <Waypoint buttomOffset={'-150px'} onEnter={this.fetchMorePhotos}>
+              <div className="loader">
+                <i className="fa fa-spinner fa-spin" />
+              </div>
+            </Waypoint>
+          ) : null}
+        </div>
+        {this.props.isCarouselShown ? (
+          <div className="overlay">
+            <PictureCarousel
+              onClose={() => this.handleCloseCarousel()}
+              index={this.props.carouselIndex}
+              photolist={this.syncPictureListState()}
+            />
+          </div>
+        ) : null}
+      </div>
+    );
+  }
 }
-
 
 export default PicturesContainer;
